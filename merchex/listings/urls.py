3231d752eda216b0abcb5e6df9608d4927c69ec5
Harlequelrah <degbovimax19@gmail.com>
2024-02-03 20:03:16 +0000

    operations update,et delete

--- merchex/listings/urls.py
+++ merchex/listings/urls.py
@@ -7,8 +7,12 @@ urlpatterns = [
     path('contact/',views.contact,name='contact'),
     path('bands/',views.band_list,name='band_list'),
     path('bands/<int:band_id>/',views.band_detail,name="band_detail"),
+    path('bands/<int:band_id>/update/',views.band_update,name="band_update"),
+    path('bands/<int:band_id>/delete/',views.band_delete,name="band_delete"),
     path('bands/add/',views.band_create,name="band_create"),
     path('listings/add/',views.listing_create,name="listing_create"),
+    path('listings/<int:listing_id>/update/',views.listing_update,name="listing_update"),
+    path('listings/<int:listing_id>/delete/',views.listing_delete,name="listing_delete"),
     path('listings/',views.listing_list,name='listing_list'),
     path('listings/<int:listing_id>/',views.listing_detail,name="listing_detail"),
 ]
--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseNotFound,HttpResponse,Http404
 from .models import Band,Listing
 from .forms import ContactUsForm,BandForm,ListingForm
 from django.core.mail import send_mail
+from django.contrib import messages
 
 
 # Create your views here.
@@ -68,6 +69,7 @@ def band_create(request):
         form=BandForm(request.POST)
         if form.is_valid():
             band=form.save()
+            messages.success(request, 'Opération réussie !')
             return redirect('band_detail',band.id)
     else:form=BandForm()
     return render(request,"band_create.html",{'form':form})
@@ -78,6 +80,50 @@ def listing_create(request):
         form=ListingForm(request.POST)
         if form.is_valid():
             listing=form.save()
+            messages.success(request, 'Opération réussie !')
             return redirect('listing_detail',listing.id)
     else:form=ListingForm()
     return render(request,"band_create.html",{'form':form})
+
+def band_update(request,band_id):
+    band=Band.objects.get(id=band_id)
+    if request.method=='POST':
+
+        form=BandForm(request.POST,instance=band)
+        if form.is_valid():
+            band=form.save()
+            messages.success(request, 'Opération réussie !')
+            return redirect('band_detail',band_id)
+    else:form=BandForm(instance=band)
+    return render(request,"band_update.html",{'form':form})
+
+
+
+def listing_update(request,listing_id):
+    listing=Listing.objects.get(id=listing_id)
+    if request.method=='POST':
+
+        form=ListingForm(request.POST,instance=listing)
+        if form.is_valid():
+            listing=form.save()
+            messages.success(request, 'Opération réussie !')
+            return redirect('listing_detail',listing_id)
+    else:form=ListingForm(instance=listing)
+    return render(request,"listing_update.html",{'form':form})
+
+def band_delete(request,band_id):
+    band=Band.objects.get(id=band_id)
+    if request.method=='POST':
+        band.delete()
+        messages.success(request, 'Opération réussie !')
+        return redirect('band_list')
+    return render(request,"band_delete.html",{'band':band})
+
+def listing_delete(request,listing_id):
+    listing=Listing.objects.get(id=listing_id)
+    if request.method=='POST':
+        listing.delete()
+        messages.success(request, 'Opération réussie !')
+        return redirect('listing_list')
+    return render(request,"listing_delete.html",{'listing':listing})
+
